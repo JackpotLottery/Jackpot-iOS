7342e38a4657741514175b7487a4b14818ad5d03
bmtreuherz <bmtreuherz@ufl.edu>
2017-07-22 18:32:17 -0700

    Initial Commit

---
+++ JackpotTests/JackpotTests.swift
@@ -0,0 +1,36 @@
+//
+//  JackpotTests.swift
+//  JackpotTests
+//
+//  Created by bradley treuherz on 7/22/17.
+//  Copyright © 2017 self. All rights reserved.
+//
+
+import XCTest
+@testable import Jackpot
+
+class JackpotTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
